ca7e5692485f5c34d1c5fa9a3d98680d7f94e242
n-shtetl <yomouschi@gmail.com>
2019-11-14 12:12:56 -0800

    Add body formatting to postShow

--- frontend/components/posts/PostShow.jsx
+++ frontend/components/posts/PostShow.jsx
@@ -11,13 +11,22 @@ class PostShow extends React.Component {
 
     render() {
         if (!this.props.post) return null;
+        const { body } = this.props.post;
+        const bodyArr = body.split("\n")
         return (
             <div className='postShow'>
                 <h1 className='postShowTitle'>{this.props.post.title}</h1>
                 <div className="postShowImageWrapper">
                     <img className='postShowImage' src={this.props.post.photoUrl}/>
                 </div>
-                <div>{this.props.post.body}</div>
+                <div>
+                    {bodyArr.map(para => (
+                        <div>
+                        <div>{para}</div>
+                        <br/>
+                        </div>
+                    ))}
+                </div>
             </div>
         )
     }
